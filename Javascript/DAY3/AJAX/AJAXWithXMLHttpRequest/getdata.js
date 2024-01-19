24fd45875f406dda705a99ab6c60fcef0f2295e7
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-01-19 14:36:19 +0530

    ✨ Handling Error in AJAX

--- Javascript/DAY3/AJAX/AJAXWithXMLHttpRequest/getdata.js
+++ Javascript/DAY3/AJAX/AJAXWithXMLHttpRequest/getdata.js
@@ -4,11 +4,13 @@ function GetData(callback) {
 
   let xmlHttpReq = new XMLHttpRequest();
 
-  xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/posts");
+  xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/postss");
 
   xmlHttpReq.onreadystatechange = function () {
     if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200) {
-      callback(xmlHttpReq.responseText);
+      callback(null, xmlHttpReq.responseText);
+    } else if (xmlHttpReq.readyState == 4 && xmlHttpReq.status !== 200) {
+      callback("Something went wrong !" + xmlHttpReq.status, null);
     }
   };
   xmlHttpReq.send();
