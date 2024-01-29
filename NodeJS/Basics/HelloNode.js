3354b3e6f33afda2332b39f27667b74acd1662a3
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-01-29 11:52:15 +0530

    ✨ understanding node modules

--- NodeJS/Basics/HelloNode.js
+++ NodeJS/Basics/HelloNode.js
@@ -1,11 +1,11 @@
 // console.log("Hello Node JS !");
 // setTimeout(() => console.log(this), 2000);
 
-const http = require("node:http");
+const http = require("http");
 const hostname = "127.0.0.1";
 const port = 3000;
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
+  res.statusCode = 500;
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello World");
 });
---
+++ NodeJS/Modules/NpmPackages/src/app.js
@@ -0,0 +1,14 @@
+var axios = require("axios");
+axios
+  .get("https://jsonplaceholder.typicode.com/posts")
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
