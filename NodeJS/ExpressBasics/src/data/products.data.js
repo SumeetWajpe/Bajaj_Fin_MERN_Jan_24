d6823d4e66d73f19556a1d87bf298981714ee105
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-01-31 09:56:39 +0530

    ✨ OnlineShopping App - GET API

--- NodeJS/ExpressBasics/src/data/products.data.js
+++ NodeJS/ExpressBasics/src/data/products.data.js
@@ -1,9 +1,48 @@
-let products = [
-  { id: 1, title: "Mac BookPro", price: 250000,rating:5,likes:400,imageUrl:"" },
-  { id: 2, title: "QLED TV", price: 80000 },
-  { id: 3, title: "Nikon 7200", price: 120000 },
-  { id: 4, title: "GoPro", price: 50000 },
-  { id: 5, title: "Nikorr200-500 Lens", price: 250000 },
+var products = [
+  {
+    id: 1,
+    title: "Mac Book Pro",
+    rating: 5,
+    price: 250000,
+    likes: 100,
+    imageUrl:
+      "https://www.apple.com/v/macbook-pro-14-and-16/b/images/overview/hero/hero_intro_endframe__e6khcva4hkeq_large.jpg",
+  },
+  {
+    id: 2,
+    title: "Mac Book Air",
+    rating: 5,
+    price: 200000,
+    likes: 200,
+    imageUrl:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1664472289661",
+  },
+  {
+    id: 3,
+    title: "Mi 4k Drone",
+    rating: 4,
+    price: 350000,
+    likes: 700,
+    imageUrl: "https://www.fimi.com/static/images/nav/top_4K.png",
+  },
+  {
+    id: 4,
+    title: "Boat Headphones",
+    rating: 4,
+    price: 2000,
+    likes: 300,
+    imageUrl:
+      "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/rockerz-518-blue_600x.png?v=1613731627",
+  },
+  {
+    id: 5,
+    title: "JBL Speakers",
+    rating: 3,
+    price: 5000,
+    likes: 800,
+    imageUrl:
+      "https://www.myg.in/images/detailed/12/JBL_Boombox_2-1_mgid-eu.jpg",
+  },
 ];
 
 module.exports = products;
--- NodeJS/ExpressBasics/static/script.js
+++ NodeJS/ExpressBasics/static/script.js
@@ -1,14 +1,51 @@
-async function LoadProducts() {
+async function FetchProducts() {
   let response = await fetch("http://localhost:3000/products");
-  let products = await response.json();
-  //   console.log(products);
-
-  let productList = document.querySelector(".productList");
-  for (const p of products) {
-    let newProductLi = document.createElement("li");
-    newProductLi.innerText = p.title;
-    productList.appendChild(newProductLi);
+  if (response.ok) {
+    let products = await response.json();
+    DisplayProducts(products);
   }
 }
+function DisplayProducts(products) {
+  // Use for-of
+  var divWrapper = document.querySelector(".productList");
 
-document.addEventListener("DOMContentLoaded", LoadProducts);
+  for (let index = 0; index < products.length; index++) {
+    divWrapper.innerHTML += CreateProductItem(products[index]);
+  }
+}
+// Use document.createElement() instead
+function CreateProductItem(theProduct) {
+  return ` <div class="col-md-3" id="${theProduct.id}">
+          <div class="card border border-1 shadow p-3 mb-5 bg-body-tertiary rounded">
+            <img
+            height="250px"
+            width="350px"
+              src="${theProduct.imageUrl}"
+              class="card-img-top"
+              alt="${theProduct.title}"
+            />
+            <div class="card-body">
+              <h5 class="card-title">${theProduct.title}</h5>
+              <p class="card-text">₹.${theProduct.price}</p>
+              <p class="card-text">
+                ${createRatings(theProduct)}
+              </p>
+               <button type="button" class="btn btn-primary"><i class="fa-solid fa-thumbs-up"></i>${
+                 theProduct.likes
+               } </button>
+            
+              </div>
+          </div>
+        </div>
+    `;
+}
+
+function createRatings(theProduct) {
+  let ratings = "";
+  for (let index = 0; index < theProduct.rating; index++) {
+    ratings += `<span style="color: orange;"><i class="fa-solid fa-star"></i></span>`;
+  }
+  return ratings;
+}
+
+window.addEventListener("DOMContentLoaded", FetchProducts);
